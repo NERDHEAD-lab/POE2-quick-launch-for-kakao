37791b2e87f1e8e77c5bf41c9cda29993fe6910e
NERDHEAD <nerdlab.kr@gmail.com>
2025-12-31 18:00:51 +0900

    fix(CI/CD):vite dev 환경 정상화

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ manifest.version = packageJson.version
 
 export default defineConfig({
   plugins: [crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
