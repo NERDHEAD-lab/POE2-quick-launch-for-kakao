49910172351d1c469c4b312724c47ac02ba3ddd4
NERDHEAD <nerdlab.kr@gmail.com>
2026-06-17 23:32:57 +0900

    fix: 카카오게임즈 URL 검증 실패 수정

--- src/constants.ts
+++ src/constants.ts
@@ -5,13 +5,13 @@ export const EXT_URLS = {
     POE: {
         HOMEPAGE: 'https://poe.kakaogames.com/',
         AUTO_START: 'https://poe.kakaogames.com#autoStart',
-        TRADE: 'https://poe.game.daum.net/trade',
+        TRADE: 'https://poe.kakaogames.com/trade',
         PATCH_NOTES_FORUM: 'https://poe.kakaogames.com/forum/view-forum/patch-notes'
     },
     POE2: {
         HOMEPAGE: 'https://pathofexile2.kakaogames.com/main',
         AUTO_START: 'https://pathofexile2.kakaogames.com/main#autoStart',
-        TRADE: 'https://poe.game.daum.net/trade2',
+        TRADE: 'https://poe.kakaogames.com/trade2',
         PATCH_NOTES_FORUM: 'https://poe.kakaogames.com/forum/view-forum/patch-notes2'
     },
     NOTICE_JSON: 'https://nerdhead-lab.github.io/POE2-quick-launch-for-kakao/notice.json'
